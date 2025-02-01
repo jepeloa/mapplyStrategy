de5ea9b11660478edf623c85178c2810cb6bd7c0
jepeloa <javier.epeloa@mapplics.com>
2025-02-01 18:42:30 -0300

    mapplySupertrend

--- mapplySupertrend.py
+++ mapplySupertrend.py
@@ -1,221 +1,166 @@
-#Mapply
-
-# --- Do not remove these libs ---
-from freqtrade.strategy.interface import IStrategy
-from pandas import DataFrame
-# --------------------------------
-
-# Add your lib to import here
-import talib.abstract as ta
-import freqtrade.vendor.qtpylib.indicators as qtpylib
-from freqtrade.strategy import (BooleanParameter, CategoricalParameter, DecimalParameter, 
-                                IStrategy, IntParameter, RealParameter)
-from scipy.spatial.distance import cosine
-import numpy as np
 import logging
-import pandas as pd
-from technical import qtpylib
+from freqtrade.strategy import IStrategy, IntParameter
 from pandas import DataFrame
-from datetime import datetime, timezone
-from typing import Optional
-from functools import reduce
-import freqtrade.vendor.qtpylib.indicators as qtpylib
+import talib.abstract as ta
+import numpy as np
 from freqtrade.persistence import Trade
-from technical import qtpylib, pivots_points
-
+import datetime
+logger = logging.getLogger(__name__)
 
 class mapplySupertrend(IStrategy):
-    use_custom_stoploss = True
-    trailing_stop = True
-    ignore_roi_if_entry_signal = True
-    use_exit_signal = True
-    minimal_roi = {
-        "0":  0.10
+    INTERFACE_VERSION: int = 3
+    
+    buy_params = {
+        "buy_m1": 4,
+        "buy_m2": 7,
+        "buy_m3": 1,
+        "buy_p1": 8,
+        "buy_p2": 9,
+        "buy_p3": 8,
     }
-    # DCA settings
-    exit_profit_only = True
-    position_adjustment_enable = True
-    max_entry_position_adjustment = 0
-    max_dca_multiplier = 1
-    stoploss = -0.25
+
+    sell_params = {
+        "sell_m1": 1,
+        "sell_m2": 3,
+        "sell_m3": 6,
+        "sell_p1": 16,
+        "sell_p2": 18,
+        "sell_p3": 18,
+    }
+
+    minimal_roi = {
+        "0": 0.05,
+        "372": 0.03,
+        "861": 0.01,
+        "2221": 0
+    }
+
+    stoploss = -0.03
+
+    trailing_stop = True
+    trailing_stop_positive = 0.05
+    trailing_stop_positive_offset = 0.144
+    trailing_only_offset_is_reached = False
+
     timeframe = '1h'
+    startup_candle_count = 200
 
-    # Number of candles the strategy requires before producing valid signals
-    startup_candle_count: int = 30 
+    buy_m1 = IntParameter(1, 7, default=4, space='buy')
+    buy_m2 = IntParameter(1, 7, default=4, space='buy')
+    buy_m3 = IntParameter(1, 7, default=4, space='buy')
+    buy_p1 = IntParameter(7, 21, default=14, space='buy')
+    buy_p2 = IntParameter(7, 21, default=14, space='buy')
+    buy_p3 = IntParameter(7, 21, default=14, space='buy')
 
-    # protections
-    cooldown_lookback = IntParameter(2, 48, default=5, space="protection", optimize=True)
-    stop_duration = IntParameter(12, 200, default=5, space="protection", optimize=True)
-    use_stop_protection = BooleanParameter(default=True, space="protection", optimize=True)
+    sell_m1 = IntParameter(1, 7, default=4, space='sell')
+    sell_m2 = IntParameter(1, 7, default=4, space='sell')
+    sell_m3 = IntParameter(1, 7, default=4, space='sell')
+    sell_p1 = IntParameter(7, 21, default=14, space='sell')
+    sell_p2 = IntParameter(7, 21, default=14, space='sell')
+    sell_p3 = IntParameter(7, 21, default=14, space='sell')
 
-    # hyperopt params
-    slope_length = IntParameter(5, 30, default=11, space="buy", optimize=True)
-    stoploss_length = IntParameter(5, 15, default=10, space="sell", optimize=True)
-    rsi_length = IntParameter(5, 14, default=14, space="buy",optimize=True)
-    rsi_buy = IntParameter(30, 60, default=55, space="buy", optimize=True)
-    fslope_buy = IntParameter(-5, 5, default=0, space="buy", optimize=True)
-    sslope_buy = IntParameter(-5, 5, default=0, space="buy", optimize=True)
-    fslope_sell = IntParameter(-5, 5, default=0, space="sell", optimize=True)
-
-    #trailing stop loss optimiziation
-    tsl_target5 = DecimalParameter(low=0.2, high=0.4, decimals=1, default=0.3, space='sell', optimize=True, load=True)
-    ts5 = DecimalParameter(low=0.04, high=0.06, default=0.05, decimals=2,space='sell', optimize=True, load=True)
-    tsl_target4 = DecimalParameter(low=0.15, high=0.2, default=0.2, decimals=2, space='sell', optimize=True, load=True)
-    ts4 = DecimalParameter(low=0.03, high=0.05, default=0.045, decimals=2,  space='sell', optimize=True, load=True)
-    tsl_target3 = DecimalParameter(low=0.10, high=0.15, default=0.15, decimals=2,  space='sell', optimize=True, load=True)
-    ts3 = DecimalParameter(low=0.025, high=0.04, default=0.035, decimals=3,  space='sell', optimize=True, load=True)
-    tsl_target2 = DecimalParameter(low=0.08, high=0.10, default=0.1, decimals=3, space='sell', optimize=True, load=True)
-    ts2 = DecimalParameter(low=0.015, high=0.03, default=0.02, decimals=3, space='sell', optimize=True, load=True)
-    tsl_target1 = DecimalParameter(low=0.05, high=0.08, default=0.06, decimals=3, space='sell', optimize=True, load=True)
-    ts1 = DecimalParameter(low=0.01, high=0.016, default=0.013, decimals=3, space='sell', optimize=True, load=True)
-    tsl_target0 = DecimalParameter(low=0.03, high=0.05, default=0.03, decimals=3, space='sell', optimize=True, load=True)
-    ts0 = DecimalParameter(low=0.008, high=0.015, default=0.013, decimals=3, space='sell', optimize=True, load=True)
-
-
- 
-
-    @property
-    def protections(self):
-        prot = []
-
-        prot.append({
-            "method": "CooldownPeriod",
-            "stop_duration_candles": self.cooldown_lookback.value
-        })
-        if self.use_stop_protection.value:
-            prot.append({
-                "method": "StoplossGuard",
-                "lookback_period_candles": 24 * 3,
-                "trade_limit": 1,
-                "stop_duration_candles": self.stop_duration.value,
-                "only_per_pair": True
-            })
-
-        return prot
-
-    ### Trailing Stop ###
-    def custom_stoploss(self, pair: str, trade: 'Trade', current_time: datetime,
-                        current_rate: float, current_profit: float, **kwargs) -> float:
-
-
-        for stop5 in self.tsl_target5.range:
-            if (current_profit > stop5):
-                for stop5a in self.ts5.range:
-                    self.dp.send_msg(f'*** {pair} *** Profit: {current_profit} - lvl5 {stop5}/{stop5a} activated')
-                    return stop5a 
-        for stop4 in self.tsl_target4.range:
-            if (current_profit > stop4):
-                for stop4a in self.ts4.range:
-                    self.dp.send_msg(f'*** {pair} *** Profit {current_profit} - lvl4 {stop4}/{stop4a} activated')
-                    return stop4a 
-        for stop3 in self.tsl_target3.range:
-            if (current_profit > stop3):
-                for stop3a in self.ts3.range:
-                    self.dp.send_msg(f'*** {pair} *** Profit {current_profit} - lvl3 {stop3}/{stop3a} activated')
-                    return stop3a 
-        for stop2 in self.tsl_target2.range:
-            if (current_profit > stop2):
-                for stop2a in self.ts2.range:
-                    self.dp.send_msg(f'*** {pair} *** Profit {current_profit} - lvl2 {stop2}/{stop2a} activated')
-                    return stop2a 
-        for stop1 in self.tsl_target1.range:
-            if (current_profit > stop1):
-                for stop1a in self.ts1.range:
-                    self.dp.send_msg(f'*** {pair} *** Profit {current_profit} - lvl1 {stop1}/{stop1a} activated')
-                    return stop1a 
-        for stop0 in self.tsl_target0.range:
-            if (current_profit > stop0):
-                for stop0a in self.ts0.range:
-                    self.dp.send_msg(f'*** {pair} *** Profit {current_profit} - lvl0 {stop0}/{stop0a} activated')
-                    return stop0a 
-
-        return self.stoploss
 
     def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        dataframe['rsi'] = ta.RSI(dataframe, timeperiod=7)
-        dataframe['marketMA'] = ta.SMA(dataframe, timeperiod=200)
-        dataframe['fastMA'] = ta.SMA(dataframe, timeperiod=21)
-        dataframe['slowMA'] = ta.SMA(dataframe, timeperiod=50)
-        dataframe['entryMA'] = ta.SMA(dataframe, timeperiod=3)
-        # Calculate slope of slowMA
-        # See: https://www.wikihow.com/Find-the-Slope-of-a-Line
-        dataframe['sy1'] = dataframe['slowMA'].shift(+(self.slope_length.value))
-        dataframe['sy2'] = dataframe['slowMA'].shift(+1)
-        sx1 = 1
-        sx2 = (self.slope_length.value)
-        dataframe['sy'] = dataframe['sy2'] - dataframe['sy1']
-        dataframe['sx'] = sx2 - sx1
-        dataframe['slow_slope'] = dataframe['sy']/dataframe['sx']
-        dataframe['fy1'] = dataframe['fastMA'].shift(+(self.slope_length.value))
-        dataframe['fy2'] = dataframe['fastMA'].shift(+1)
-        fx1 = 1
-        fx2 = (self.slope_length.value)
-        dataframe['fy'] = dataframe['fy2'] - dataframe['fy1']
-        dataframe['fx'] = fx2 - fx1
-        dataframe['fast_slope'] = dataframe['fy']/dataframe['fx']
-        # print(dataframe[['date','close', 'slow_slope','fast_slope']].tail(50))
+        # Calcular supertrends para la configuración actual
+        supertrend_1_buy = self.supertrend(dataframe, self.buy_m1.value, self.buy_p1.value)
+        supertrend_2_buy = self.supertrend(dataframe, self.buy_m2.value, self.buy_p2.value)
+        supertrend_3_buy = self.supertrend(dataframe, self.buy_m3.value, self.buy_p3.value)
 
-        # ==== Trailing custom stoploss indicator ====
-        dataframe['last_lowest'] = dataframe['low'].rolling((self.stoploss_length.value)).min().shift(1)
+        supertrend_1_sell = self.supertrend(dataframe, self.sell_m1.value, self.sell_p1.value)
+        supertrend_2_sell = self.supertrend(dataframe, self.sell_m2.value, self.sell_p2.value)
+        supertrend_3_sell = self.supertrend(dataframe, self.sell_m3.value, self.sell_p3.value)
 
-        for valb in self.rsi_length.range:
-            dataframe[f'rsi_{valb}'] = ta.RSI(dataframe, timeperiod=valb)
+        dataframe['supertrend_1_buy'] = supertrend_1_buy['STX']
+        dataframe['supertrend_2_buy'] = supertrend_2_buy['STX']
+        dataframe['supertrend_3_buy'] = supertrend_3_buy['STX']
 
+        dataframe['supertrend_1_sell'] = supertrend_1_sell['STX']
+        dataframe['supertrend_2_sell'] = supertrend_2_sell['STX']
+        dataframe['supertrend_3_sell'] = supertrend_3_sell['STX']
+
+        # EMA 200 (para filtro de tendencia, ajustado a 20 como en el código original)
+        dataframe['ema_200'] = ta.EMA(dataframe, timeperiod=20)
+
+        # ADX + DI+ y DI-
+        dataframe['ADX'] = ta.ADX(dataframe, timeperiod=14)
+        dataframe['DI_plus'] = ta.PLUS_DI(dataframe, timeperiod=14)
+        dataframe['DI_minus'] = ta.MINUS_DI(dataframe, timeperiod=14)
+        dataframe['rsi'] = ta.RSI(dataframe)
+        rsi = 0.1 * (dataframe['rsi'] - 50)
+        dataframe['fisher_rsi'] = (np.exp(2 * rsi) - 1) / (np.exp(2 * rsi) + 1)
+        dataframe['sar'] = ta.SAR(dataframe)
         return dataframe
 
-    plot_config = {
-        "main_plot": {
-            # Configuration for main plot indicators.
-            "fastMA": {"color": "red"},
-            "slowMA": {"color": "blue"},
-        },
-        "subplots": {
-            # Additional subplots
-            "rsi": {"rsi": {"color": "blue"}},
-            "fast_slope": {"fast_slope": {"color": "red"}, "slow_slope": {"color": "blue"}},
-        },
-    }
-
-
     def populate_entry_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        # Filtro ADX: Por ejemplo, ADX > 50 para mayor fortaleza de tendencia
+        adx_threshold = 0
+
         dataframe.loc[
             (
-                # Only enter when market is bullish (this is a choice)
-                (
-                 (dataframe['close'] > dataframe['marketMA']) &
-                # Only trade when the fast slope is above 0
-                (dataframe['fast_slope'] > self.fslope_buy.value) &
-                # Only trade when the slow slope is above 0
-                (dataframe['slow_slope'] > self.sslope_buy.value) &
-                # Only buy when the close price is higher than the 3day average of ten periods ago
-                # (dataframe['close'] > dataframe['entryMA'].shift(+(slope_length))) &
-                # Or only buy when the close price is higher than the close price of 3 days ago (this is a choice)
-                # (qtpylib.crossed_above(dataframe['close'], dataframe['close'].shift(+(self.slope_length.value)))) &
-                (dataframe['close'] > dataframe['close'].shift(+(self.slope_length.value))) &
-                # Only enter trades when the RSI is higher than 55
-                (dataframe[f'rsi_{self.rsi_length.value}'] > self.rsi_buy.value) 
-                # Only trade when the fast MA is above the slow MA
-                # (dataframe['fastMA'] > dataframe['slowMA'])
-                # Or trade when the fase MA crosses above the slow MA (This is a choice...)
-                # (qtpylib.crossed_above(dataframe['fastMA'], dataframe['slowMA']))
-                )
+               (dataframe['supertrend_1_sell'] == 'up') &
+               (dataframe['supertrend_2_sell'] == 'up') &
+               (dataframe['supertrend_3_sell'] == 'up') &
+               (dataframe['ADX'] > adx_threshold) &
+               #(dataframe['DI_plus'] > dataframe['DI_minus']) &
+               (dataframe['volume'] > 0)
             ),
-            'buy'] = 1
-
+            'enter_long'] = 1
 
         return dataframe
 
     def populate_exit_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
-        dataframe.loc[
-            (
+        # Actualmente sin condiciones específicas, se puede mejorar.
+        #dataframe.loc[
+         #   (
+               #(dataframe['supertrend_1_sell'] == 'down') &
+               #(dataframe['supertrend_2_sell'] == 'down') &
+               #(dataframe['supertrend_3_sell'] == 'down') &
+               #(dataframe['ADX'] > adx_threshold) &
+               #(dataframe['DI_plus'] > dataframe['DI_minus']) &
+          #     (dataframe['volume'] > 0)
+           # ),
+            #'exit_long'] = 1
 
-                # Close or do not trade when fastMA is below slowMA # modified from strategy
-                (dataframe['fast_slope'] < self.fslope_sell.value)
-                # Or close position when the close price gets below the last lowest candle price configured
-                # (AKA candle based (Trailing) stoploss) 
-                | (dataframe['close'] < dataframe['last_lowest'])
-                # | (dataframe['fastMA'] < dataframe['slowMA'])
-                # | (dataframe['close'] < dataframe['fastMA'])
-            ),
-            'sell'] = 1
         return dataframe
+
+    def supertrend(self, dataframe: DataFrame, multiplier, period):
+        df = dataframe.copy()
+
+        df['TR'] = ta.TRANGE(df)
+        df['ATR'] = ta.SMA(df['TR'], period)
+
+        st = f'ST_{period}_{multiplier}'
+        stx = f'STX_{period}_{multiplier}'
+
+        df['basic_ub'] = (df['high'] + df['low']) / 2 + multiplier * df['ATR']
+        df['basic_lb'] = (df['high'] + df['low']) / 2 - multiplier * df['ATR']
+
+        df['final_ub'] = 0.00
+        df['final_lb'] = 0.00
+
+        for i in range(period, len(df)):
+            df['final_ub'].iat[i] = (df['basic_ub'].iat[i] if df['basic_ub'].iat[i] < df['final_ub'].iat[i - 1] or df['close'].iat[i - 1] > df['final_ub'].iat[i - 1]
+                                     else df['final_ub'].iat[i - 1])
+            df['final_lb'].iat[i] = (df['basic_lb'].iat[i] if df['basic_lb'].iat[i] > df['final_lb'].iat[i - 1] or df['close'].iat[i - 1] < df['final_lb'].iat[i - 1]
+                                     else df['final_lb'].iat[i - 1])
+
+        df[st] = 0.00
+        for i in range(period, len(df)):
+            if df[st].iat[i - 1] == df['final_ub'].iat[i - 1] and df['close'].iat[i] <= df['final_ub'].iat[i]:
+                df[st].iat[i] = df['final_ub'].iat[i]
+            elif df[st].iat[i - 1] == df['final_ub'].iat[i - 1] and df['close'].iat[i] > df['final_ub'].iat[i]:
+                df[st].iat[i] = df['final_lb'].iat[i]
+            elif df[st].iat[i - 1] == df['final_lb'].iat[i - 1] and df['close'].iat[i] >= df['final_lb'].iat[i]:
+                df[st].iat[i] = df['final_lb'].iat[i]
+            elif df[st].iat[i - 1] == df['final_lb'].iat[i - 1] and df['close'].iat[i] < df['final_lb'].iat[i]:
+                df[st].iat[i] = df['final_ub'].iat[i]
+
+        df[stx] = np.where((df[st] > 0.00), np.where((df['close'] < df[st]), 'down', 'up'), np.NaN)
+
+        df.drop(['basic_ub', 'basic_lb', 'final_ub', 'final_lb'], inplace=True, axis=1)
+        df.fillna(0, inplace=True)
+
+        return DataFrame(index=df.index, data={
+            'ST': df[st],
+            'STX': df[stx]
+        })
